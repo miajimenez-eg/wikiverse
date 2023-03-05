31bfb4d395636f7eb79f0f22a2d6557bbb421b13
Mia Jimenez <miajimenez@expediagroup.com>
2023-03-05 19:03:44 +0000

    add click handlers to pageslist

--- public/react/components/PagesList.js
+++ public/react/components/PagesList.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { Page } from './Page';
 
 export const PagesList = ({pages}) => {
+
+	const handlePageClick = async (slug) => {
+		const res = await fetch(`${apiURL}/wiki/${slug}`);
+		const data = await res.json();
+		setSelectedPage(data);
+	}
+
+	const handleBackClick = async () => {
+		setSelectedPage(null);
+	}
+
 	return <>
 		{
 			pages.map((page, idx) => {
